53c61ff412e406c278260c53b5fbdbfef03dadea
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-03-09 17:52:18 +0700

    Add files via upload
    การบ้านครั้งที่ 3 (Conditional Statement) วิชา MDT112 จัดทำโดย นายกันตพัฒน์ ขวัญสมคิด 63120501002

---
+++ MDT112_HW3-Conditional Statement_63120501002.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MDT112_HW3_Conditional_Statement_63120501002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ModeProgram = Console.ReadLine();
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            if (ModeProgram = "time")
+            {
+                Console.WriteLine();
+            }
+            if (ModeProgram = "price")
+            {
+                Console.WriteLine();
+            }
+            else (ModeProgram = "")
+            {
+                Console.WriteLine("Invalid mode");
+            }
+
+            if (x < 1)
+            {
+                Console.WriteLine();
+            }
+            else if(y > 1)
+            {
+                Console.WriteLine("lnvalid mouse position");
+            }
+
+            Console.ReadLine();
+             
+        }
+    }
+}
